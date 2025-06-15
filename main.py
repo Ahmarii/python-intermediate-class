1e8aed80275415c8511efbbfd5248d52e5ad513a
DESKTOP-1ELE0D0\Admin <test>
2025-06-15 11:01:52 +0700

    add method

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 class Student:
-    def __init__(self, name, age, courses):
-        self.name = name
+    def __init__(self, fname,lname, age, courses):
+        self.fname = fname
+        self.lname = lname
         self.age = age
         self.courses = courses
+    
+    #Method
+    def get_fullname(self):
+        return f"{self.fname} {self.lname}"
+    
     #Function -> Method
     def display_info(self):
-        print("Name:", self.name)
+        print("Name:", self.get_fullname())
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print("-" * 20)
@@ -15,3 +21,5 @@ student2 = Student("Bob","Builder", 22, ["Physics", "History"])
 
 student1.display_info()
 student2.display_info()
+
+student1.get_fullname()
